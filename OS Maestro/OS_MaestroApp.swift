cd2559cdadd571e716071a9b2edd5a5f39e7b26e
Zac <97338659+ZProLegend007@users.noreply.github.com>
2024-05-02 21:09:42 +0800

    Initial Commit

---
+++ OS Maestro/OS_MaestroApp.swift
@@ -0,0 +1,17 @@
+//
+//  OS_MaestroApp.swift
+//  OS Maestro
+//
+//  Created by Zac Booth on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct OS_MaestroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
